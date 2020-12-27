b778ecbfc53691a6a0429033d5b73c22f4ce0166
valentin dupas <valentin.lc.dupas@gmail.com>
2020-12-27 21:49:05 +0100

    upgrade from electron 8 to 11

--- src/index.js
+++ src/index.js
@@ -20,7 +20,8 @@ const createWindow = () => {
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true
+      nodeIntegration: true,
+      contextIsolation: false
     }
   });
 
